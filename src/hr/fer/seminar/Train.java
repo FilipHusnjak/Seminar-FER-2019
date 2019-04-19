f916cdfab0a78c028cc6094ec79e9083daa3db32
Filip Husnjak <filip.husnjak@fer.hr>
2019-04-19 19:51:56 +0200

    Perceptron class added.

--- src/hr/fer/seminar/Train.java
+++
@@ -1,5 +0,0 @@
-package hr.fer.seminar;
-
-public class Train {
-	
-}
--- src/hr/fer/seminar/Window.java
+++ src/hr/fer/seminar/Window.java
@@ -9,7 +9,6 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -24,20 +23,14 @@ public class Window extends JFrame {
 	private static final int DRAW_WIDTH = 420;
 	private static final int DRAW_HEIGHT = 420;
 	
-	private static final int NUM_INPUTS = 3;
-	
 	private static final int DESIRED_RED = -1;
 	private static final int DESIRED_BLUE = 1;
-	
-	private static final double learningRate = 0.1;
 		
 	private DrawPanel drawPanel = new DrawPanel();
 	
 	private List<Coordinate> coordinatesRed = new ArrayList<>();
 	private List<Coordinate> coordinatesBlue = new ArrayList<>();
-	
-	private double[] weights = new double[NUM_INPUTS];
-	
+		
 	List<List<Coordinate>> listCoord = Arrays.asList(coordinatesRed, coordinatesBlue);
 	
 	private boolean running = false;
@@ -46,48 +39,39 @@ public class Window extends JFrame {
 	private Color[] colors = {Color.RED, Color.BLUE};
 	private int colorIndex = 0;
 	
+	Perceptron perceptron = new Perceptron();
+	
 	public Window() {
-		
-		super();
-		
-		setup();
-		
-		this.setLocation(20, 20);
-		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		
-		this.setTitle("Simulation");
-		
-		this.setLayout(new BorderLayout());
-		
-		this.add(drawPanel, BorderLayout.CENTER);
+		super.setLocation(20, 20);
+		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		super.setTitle("Simulation");
+		super.setLayout(new BorderLayout());
+		super.add(drawPanel, BorderLayout.CENTER);
 		
 		JPanel panelSouth = new JPanel();
 		JButton btnStart = new JButton("Start");
 		JButton btnStop = new JButton("Stop");
 		JButton btnColor = new JButton("Color");
 		JButton btnClear = new JButton("Clear");
+		
 		btnColor.setBackground(Color.RED);
 		
 		drawPanel.addMouseListener(new MouseAdapter() {
 			
 			@Override
             public void mousePressed(MouseEvent e) {
-				
 				listCoord.get(colorIndex).add(new Coordinate(e.getPoint().x, e.getPoint().y));
                 drawPanel.repaint();
-                
             }
 			
 		});
 		
 		btnClear.addActionListener(e -> {
-			
-			setup();
-			
+			perceptron.setup();
 			coordinatesRed.clear();
 			coordinatesBlue.clear();
-			running = false;
 			clear = true;
+			running = false;
 			drawPanel.repaint();
 			
 		});
@@ -112,9 +96,7 @@ public class Window extends JFrame {
 		});
 		
 		btnStop.addActionListener(e -> {
-			
-			setup();
-			
+			perceptron.setup();
 			clear = true;
 			running = false;
 			drawPanel.repaint();
@@ -137,34 +119,14 @@ public class Window extends JFrame {
 	}
 	
 	
-	
-	public void setup() {
-		Random random = new Random();
-		for (int i = 0; i < NUM_INPUTS; ++i) {
-			weights[i] = random.nextDouble();
-		}
-	}
-	
-	
-	
 	private boolean train() {
-		
 		boolean trained = true;
 		trained = loopCoordinates(coordinatesRed, DESIRED_RED);
-		trained = loopCoordinates(coordinatesBlue, DESIRED_BLUE);
-		
-		for (int i = 0; i < NUM_INPUTS; ++i) {
-			System.out.print(weights[i] + " ");
-		}
-		System.out.println();
-		
-		return trained;
+		return trained &&  loopCoordinates(coordinatesBlue, DESIRED_BLUE);
 	}
-	
 	
 	
 	private boolean loopCoordinates(List<Coordinate> coordinates, int desiredValue) {
-		
 		boolean trained = true;
 		for (Coordinate c : coordinates) {
 			double xCord = (double) c.x / DRAW_WIDTH * 2 - 1;
@@ -173,36 +135,15 @@ public class Window extends JFrame {
 			double[] inputs = new double[] {1, xCord, yCord};
 			int error = match(inputs, desiredValue);
 			if (error != 0) trained = false;
-			adjustWeight(inputs, error);
+			perceptron.adjustWeight(inputs, error);
 		}
-		
 		return trained;
 	}
 	
 	
-	
 	private int match(double[] inputs, int desiredOut) {
-		return desiredOut - guess(inputs);
+		return desiredOut - perceptron.guess(inputs);
 	}
-	
-	
-	
-	private int guess(double[] inputs) {
-		double weightedSum = 0;
-		for (int i = 0; i < inputs.length; ++i) {
-			weightedSum += inputs[i] * weights[i];
-		}
-		return (weightedSum > 0 ? DESIRED_BLUE : DESIRED_RED);
-	}
-	
-	
-	
-	private void adjustWeight(double[] inputs, int error) {
-		for (int i = 0; i < NUM_INPUTS; ++i) {
-			weights[i] += inputs[i] * error * learningRate;
-		}
-	}
-	
 	
 	
 	private class DrawPanel extends JPanel {
@@ -222,7 +163,7 @@ public class Window extends JFrame {
             		double xCord = (double) i / DRAW_WIDTH * 2 - 1;
         			double yCord = (double) j / DRAW_HEIGHT * 2 - 1;
         			
-            		if (guess(new double[] {1, xCord, yCord}) > 0) {
+            		if (perceptron.guess(new double[] {1, xCord, yCord}) > 0) {
             			g.setColor(Color.BLUE);
             		} else {
             			g.setColor(Color.RED); 
@@ -247,7 +188,6 @@ public class Window extends JFrame {
             }
         }
     }
-	
 	
 	
 	private static class Coordinate {
